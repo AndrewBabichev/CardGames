e47451c73b5d2a649b991e970e0f990be14187be
AndrewBabichev <79378003+AndrewBabichev@users.noreply.github.com>
2021-05-17 20:02:35 +0300

    Added flake8 and pydocstyle support

--- cards.py
+++ cards.py
@@ -1,3 +1,5 @@
+"""Module for common card entities and operations."""
+
 import numpy as np
 import tkinter as tk
 
@@ -6,32 +8,79 @@ from playsound import playsound
 
 
 class Cards(tk.Frame):
-    def __init__(self, parent = None):
+    """Class for basic operation with cards."""
+
+    def __init__(self, parent=None):
+        """
+        Initialize all card`s images.
+
+        Args:
+            parent: parameter to be passed to tkinter.Frame constructor
+        """
         super().__init__(parent)
-        self.cards_images = np.zeros((4, 13), dtype = tk.PhotoImage) #0 - Hearts (Черви), 1 - Diamonds (Буби), 2 - Clubs (Крести), 3 - Spades (Пики)
+        # 0 - Hearts, 1 - Diamonds, 2 - Clubs, 3 - Spades
+        self.cards_images = np.zeros((4, 13), dtype=tk.PhotoImage)
         self.card_height, self.card_width = 230, 145
 
         for i in range(self.cards_images.shape[0]):
             for j in range(self.cards_images.shape[1]):
                 name = './resourses/cards/' + str(i) + '_' + str(j) + '.png'
-                self.cards_images[i, j] = tk.PhotoImage(file = name)
-        self.empty_image = tk.PhotoImage(file = './resourses/cards/empty.png')
-        self.back_image = tk.PhotoImage(file = './resourses/cards/back.png')
+                self.cards_images[i, j] = tk.PhotoImage(file=name)
+        self.empty_image = tk.PhotoImage(file='./resourses/cards/empty.png')
+        self.back_image = tk.PhotoImage(file='./resourses/cards/back.png')
 
-    def create_button(self, cards_id, position, relief = 'groove'):
-        if cards_id is None:
-            button = tk.Button(self.master, image = self.empty_image, relief = relief)
-        elif cards_id == -1:
-            button = tk.Button(self.master, image = self.back_image, relief = relief)
+    def create_button(self, card_id, position, relief='groove'):
+        """
+        Create specified card`s button in specified position.
+
+        Args:
+        card_id: either list [card_suit, card_value] or -1 for blank space or
+            None for back of cards
+        position: list [desired y coordinate, desired x coordinate]
+
+        Returns:
+            tkinter.Button: created button
+
+        """
+        if card_id is None:
+            button = tk.Button(
+                self.master,
+                image=self.empty_image,
+                relief=relief
+            )
+        elif card_id == -1:
+            button = tk.Button(
+                self.master,
+                image=self.back_image,
+                relief=relief
+            )
         else:
-            (i, j) = cards_id
-            button = tk.Button(self.master, image = self.cards_images[i, j], relief = relief)
-        button.place(y = position[0], x = position[1])
+            (i, j) = card_id
+            button = tk.Button(
+                self.master,
+                image=self.cards_images[i, j],
+                relief=relief
+            )
+        button.place(y=position[0], x=position[1])
         return button
 
 
 class Deck(Cards):
-    def __init__(self, parent = None, row = 0, hands = None, player_hand = 0, expand = False):
+    """Base class for game handlers."""
+
+    def __init__(
+            self, parent=None, row=0,
+            hands=None, player_hand=0, expand=False):
+        """
+        Create card deck and initialize common game parameters.
+
+        Args:
+            parent: parameter to be passed to tkinter.Frame constructor
+            row (int): row to visualize deck on
+            hands: card hands to handle
+            player_hand: index to player hand
+            expand: whether to expand card hand or to keep it to 6 cards
+        """
         super().__init__(parent)
         self.row = row
         self.hands = hands
@@ -40,64 +89,96 @@ class Deck(Cards):
 
         self.deck_cards_ids = np.arange(52)
         self.deck_cards = []
-        self.cards = []
         self.update()
 
     def update(self):
+        """Update and visualize deck cards."""
         for deck_card in self.deck_cards:
             deck_card.destroy()
 
-        self.deck_cards = []
-        self.deck_cards.append(tk.Button(self.master, image = self.empty_image, state = 'disabled'))
-        self.deck_cards[-1].place(y = self.row * self.card_height, x = 0)
-        #self.deck_cards[-1].bind('<Button-1>', partial(self.draw, self.hands[self.player_hand], 6, 5, True))
-        #self.deck_cards[-1].bind('<Button-2>', self.shuffle)
-        self.deck_cards[-1].bind('<Button-3>', partial(self.get, self.hands[self.player_hand]))
+        self.deck_cards.clear()
+        self.deck_cards.append(
+            tk.Button(self.master, image=self.empty_image, state='disabled')
+        )
+        self.deck_cards[-1].place(
+            y=self.row * self.card_height,
+            x=0
+        )
+        self.deck_cards[-1].bind(
+            '<Button-3>',
+            partial(self.get, self.hands[self.player_hand])
+        )
 
         for i in range(int(np.ceil(self.deck_cards_ids.size / 13))):
-            self.deck_cards.append(tk.Button(self.master, image = self.back_image, state = 'disabled'))
-            self.deck_cards[-1].place(y = self.row * self.card_height - i*3, x = -i*3)
-            #self.deck_cards[-1].bind('<Button-1>', partial(self.draw, self.hands[self.player_hand], 6, 5, True))
-            #self.deck_cards[-1].bind('<Button-2>', self.shuffle)
-            self.deck_cards[-1].bind('<Button-3>', partial(self.get, self.hands[self.player_hand]))
+            self.deck_cards.append(
+                tk.Button(self.master, image=self.back_image, state='disabled')
+            )
+            self.deck_cards[-1].place(
+                y=self.row * self.card_height - i*3,
+                x=-i*3
+            )
+            self.deck_cards[-1].bind(
+                '<Button-3>',
+                partial(self.get, self.hands[self.player_hand])
+            )
 
-    def draw(self, hand, amount = 6, empty = 0, drop = False, event = None):
+    def draw(self, hand, amount=6, empty=0, drop=False, event=None):
+        """
+        Draw desired amount of cards to specified hand and visualize them.
+
+        Args:
+            hand: card hand to draw to
+            amount: total amount of cards to draw
+            empty: number of drawed card to be empty
+            drop: whether to return drawed cards to deck or not
+        """
         if self.deck_cards_ids.size > 0:
-            cards_id = []
+            playsound('./resourses/sounds/draw.wav')
+            cards_ids = []
             for i in range(amount - empty):
                 card_id = np.random.choice(self.deck_cards_ids, (1))
                 if drop:
-                    self.deck_cards_ids = np.setdiff1d(self.deck_cards_ids, card_id)
-                cards_id.append([card_id[0] // 13, card_id[0] % 13])
-            playsound('./resourses/sounds/draw.wav')
+                    self.deck_cards_ids = np.setdiff1d(
+                        self.deck_cards_ids,
+                        card_id
+                    )
+                cards_ids.append([card_id[0] // 13, card_id[0] % 13])
 
             for i in range(empty):
-                cards_id.append(None)
-            hand.show(cards_id)
-
+                cards_ids.append(None)
+            hand.show(cards_ids)
             self.update()
 
-    def get(self, hand, event = None):
+    def get(self, hand, event=None):
+        """
+        Draw one card to specified hand and visualize it.
+
+        Args:
+            hand: card hand to draw to
+        """
         if None in hand.cards_ids and self.deck_cards_ids.size > 0:
             playsound('./resourses/sounds/draw.wav')
             index = hand.cards_ids.index(None)
             card_id = np.random.choice(self.deck_cards_ids, (1))
             self.deck_cards_ids = np.setdiff1d(self.deck_cards_ids, card_id)
+
             i, j = card_id[0] // 13, card_id[0] % 13
             hand.cards_ids[index] = [i, j]
             hand.show(hand.cards_ids)
             self.update()
 
-        elif self.deck_cards_ids.size > 0 and self.expand:
+        elif self.expand and self.deck_cards_ids.size > 0:
             playsound('./resourses/sounds/draw.wav')
             card_id = np.random.choice(self.deck_cards_ids, (1))
             self.deck_cards_ids = np.setdiff1d(self.deck_cards_ids, card_id)
+
             i, j = card_id[0] // 13, card_id[0] % 13
             hand.cards_ids.append([i, j])
             hand.show(hand.cards_ids)
             self.update()
 
-    def shuffle(self, event = None):
+    def shuffle(self, event=None):
+        """Reset the deck and card hands."""
         playsound('./resourses/sounds/shuffle.wav')
         self.deck_cards_ids = np.arange(52)
         self.update()
@@ -107,7 +188,20 @@ class Deck(Cards):
 
 
 class Hand(Cards):
-    def __init__(self, parent = None, row = 0, show_cards = True, allow_movement = True):
+    """Class that simulates actions of a card hand."""
+
+    def __init__(
+            self, parent=None, row=0,
+            show_cards=True, allow_movement=True):
+        """
+        Initialize card hand parameters.
+
+        Args:
+            parent: parameter to be passed to tkinter.Frame constructor
+            row (int): row to visualize hand on
+            show_cards: whether to show cards or their backs
+            allow_movement: whether to allow to move cards within the hand
+        """
         super().__init__(parent)
         self.row = row
         self.show_cards = show_cards
@@ -118,21 +212,43 @@ class Hand(Cards):
         self.change_index = -1
 
     def position(self, column):
+        """
+        Return card`s position based on its column.
+
+        Args:
+            column: desired place in card hand (0 .. len(cards_ids) - 1)
+        """
         if len(self.cards_ids) <= 6:
             return [self.row * self.card_height, column * self.card_width]
         else:
-            return [self.row * self.card_height, np.floor(column * self.card_width * 5 / (len(self.cards_ids) - 1))]
+            return [self.row * self.card_height, np.floor(
+                column * self.card_width * 5 / (len(self.cards_ids) - 1))
+            ]
 
-    def choose_card(self, index, event):
+    def choose_card(self, index, event=None):
+        """
+        Highlight or swap specified card.
+
+        Args:
+            index: index to hand`s card to perform action on
+        """
         if self.change_index == -1:
             self.cards[index].destroy()
-            self.cards[index] = self.create_button(self.cards_ids[index], self.position(index), 'sunken')
-            self.cards[index].bind('<Button-1>', partial(self.choose_card, index))
+            self.cards[index] = self.create_button(
+                self.cards_ids[index],
+                self.position(index),
+                'sunken'
+            )
+            self.cards[index].bind(
+                '<Button-1>',
+                partial(self.choose_card, index)
+            )
             self.change_index = index
         else:
             if self.change_index != index:
                 playsound('./resourses/sounds/playcard.wav')
-                self.cards_ids[index], self.cards_ids[self.change_index] = self.cards_ids[self.change_index], self.cards_ids[index]
+                self.cards_ids[index], self.cards_ids[self.change_index] = \
+                    self.cards_ids[self.change_index], self.cards_ids[index]
                 self.show(self.cards_ids)
             else:
                 self.show(self.cards_ids)
@@ -140,19 +256,30 @@ class Hand(Cards):
             self.change_index = -1
 
     def show(self, cards_ids):
-        if len(self.cards_ids) != 0:
-            for card in self.cards:
-                card.destroy()
-            self.cards = list()
+        """
+        Visualize specified cards.
+
+        Args:
+            cards_ids: card`s identifiers to visualize
+        """
+        for card in self.cards:
+            card.destroy()
+        self.cards.clear()
 
         self.cards_ids = cards_ids
 
         if len(cards_ids) != 0:
-
-            for column, cards_id in enumerate(cards_ids):
-                if not self.show_cards and cards_id is not None:
-                    self.cards.append(self.create_button(-1, self.position(column)))
+            for column, card_id in enumerate(cards_ids):
+                if not self.show_cards and card_id is not None:
+                    self.cards.append(
+                        self.create_button(-1, self.position(column))
+                    )
                 else:
-                    self.cards.append(self.create_button(cards_id, self.position(column)))
+                    self.cards.append(
+                        self.create_button(card_id, self.position(column))
+                    )
                 if self.allow_movement:
-                    self.cards[-1].bind('<Button-1>', partial(self.choose_card, column))
+                    self.cards[-1].bind(
+                        '<Button-1>',
+                        partial(self.choose_card, column)
+                    )
--- queen.py
+++ queen.py
@@ -1,20 +1,44 @@
+"""Module for queen card game."""
+
 import numpy as np
 import tkinter as tk
 
 from functools import partial
+from playsound import playsound
 from tkinter import messagebox
 
-from cards import *
+from cards import Hand, Deck
 
 
 class Queen_Hand(Hand):
-    def __init__(self, parent = None, row = 0, show_cards = True, allow_movement = True):
+    """Extended hand class with queen game features."""
+
+    def __init__(
+            self, parent=None, row=0,
+            show_cards=True, allow_movement=True):
+        """
+        Initialize card hand parameters.
+
+        Args:
+            parent: parameter to be passed to tkinter.Frame constructor
+            row (int): row to visualize hand on
+            show_cards: whether to show cards or their backs
+            allow_movement: whether to allow to move cards within the hand
+        """
         super().__init__(parent, row, show_cards, allow_movement)
 
-    def choose_card(self, index, event):
-        if (self.change_index != -1 and self.cards_ids[index] != self.cards_ids[self.change_index] and
-            self.cards_ids[index][1] == self.cards_ids[self.change_index][1] and
-            self.cards_ids[index] != [3, 11] and self.cards_ids[self.change_index] != [3, 11]):
+    def choose_card(self, index, event=None):
+        """
+        Either discard same values cards or perform base class actions.
+
+        Args:
+            index: index to hand`s card to perform action on
+        """
+        card_id_1, card_id_2 = \
+            self.cards_ids[index], self.cards_ids[self.change_index]
+        if (self.change_index != -1 and
+                card_id_1 != card_id_2 and card_id_1[1] == card_id_2[1] and
+                card_id_1 != [3, 11] and card_id_2 != [3, 11]):
 
             playsound('./resourses/sounds/Passturn.wav')
             self.cards_ids.pop(max(index, self.change_index))
@@ -22,116 +46,214 @@ class Queen_Hand(Hand):
             self.show(self.cards_ids)
             self.change_index = -1
             if len(self.cards_ids) == 0:
-                messagebox.showinfo(message = "Win")
+                messagebox.showinfo(message="Win")
                 Q.master.after(200, Q.new_game())
         else:
-            super().choose_card(index, None)
+            super().choose_card(index)
+
 
 class Queen(Deck):
-    def __init__(self, parent = None, row = 0, hands = None, player_hand = 0, expand = True):
+    """Handler for queen game."""
 
+    def __init__(
+            self, parent=None, row=0,
+            hands=None, player_hand=0, expand=True):
+        """
+        Create deck and buttons, initialize queen params and start new game.
+
+        Args:
+            parent: parameter to be passed to tkinter.Frame constructor
+            row: row to visualize deck on
+            hands: card hands to handle
+            player_hand: index to player hand
+            expand: whether to expand card hand or to keep it to 6 cards
+        """
         super().__init__(parent, row, hands, player_hand, expand)
-        self.enemy_hand = 1 - self.player_hand
 
-        self.ready_button = tk.Button(parent, text = 'Ready', font = 40, command = self.pass_turn)
-        self.ready_button.place(y = row * self.card_height + 90, x = 800)
-
+        self.ready_button = tk.Button(
+            parent,
+            text='Ready',
+            font=40,
+            command=self.pass_turn
+        )
+        self.ready_button.place(y=row * self.card_height + 90, x=800)
         self.new_game()
 
-    def unfold(self, index = None, event = None):
+    def unfold(self, index, event=None):
+        """
+        Unfold chosen card and place it to player hand.
+
+        Args:
+            index: index to chosen card
+        """
         if self.stage == 0:
             playsound('./resourses/sounds/untap.wav')
             position = self.hands[self.enemy_hand].position(index)
             card_id = self.hands[self.enemy_hand].cards_ids.pop(index)
-            if len(self.hands[self.enemy_hand].cards_ids) == 0:
-                messagebox.showinfo(message = "Lose")
-                self.new_game()
-            else:
-                self.hands[self.enemy_hand].cards[index].destroy()
-                self.hands[self.enemy_hand].cards[index] = self.create_button(card_id, position)
-                self.hands[self.player_hand].cards_ids.append(card_id)
-                self.stage = 1
-
-                self.master.after(200, self.unfold)
+            self.hands[self.enemy_hand].cards[index].destroy()
+            self.hands[self.enemy_hand].cards[index] = self.create_button(
+                card_id,
+                position
+            )
+            self.hands[self.player_hand].cards_ids.append(card_id)
+            self.stage = 1
+            self.master.after(200, self.unfold, -1)
         elif self.stage == 1:
             for hand in self.hands:
                 hand.show(hand.cards_ids)
             for i, card in enumerate(self.hands[self.enemy_hand].cards):
                 card.bind('<Button-1>', partial(self.unfold, i))
-            self.stage = 2
+            self.end_game(2)
 
-    def find_pairs(self, hand):
+    def _find_pairs(self, hand):
+        """
+        Find indices of cards with same values.
+
+        Args:
+            hand: card hand to perform search on
+
+        Return:
+            either sorted tuple of indices or -1 if there are no pairs
+        """
         for i in range(len(hand.cards_ids)):
             for j in range(i + 1, len(hand.cards_ids)):
-                if hand.cards_ids[i][1] == hand.cards_ids[j][1] and hand.cards_ids[i] != [3, 11] and hand.cards_ids[j] != [3, 11]:
+                card_id_1, card_id_2 = \
+                    hand.cards_ids[i], hand.cards_ids[j]
+                if (card_id_1[1] == card_id_2[1] and
+                        card_id_1 != [3, 11] and card_id_2 != [3, 11]):
                     return max(i, j), min(i, j)
         return -1
 
     def remove_pairs(self, hand):
-        flag = self.find_pairs(hand)
+        """
+        Remove all card pairs with same values except for queen of spades.
+
+        Args:
+            hand: card hand to remove cards from
+        """
+        flag = self._find_pairs(hand)
         if flag != -1:
             playsound('./resourses/sounds/Passturn.wav')
         while flag != -1:
             hand.cards_ids.pop(flag[0])
             hand.cards_ids.pop(flag[1])
-            flag = self.find_pairs(hand)
+            flag = self._find_pairs(hand)
 
-    def pass_turn(self, event = None):
+    def end_game(self, stage=0):
+        """
+        Check if game is finished otherwise set game stage.
+
+        Args:
+            stage: stage to set after function
+        """
+        if len(self.hands[self.player_hand].cards_ids) == 0:
+            messagebox.showinfo(message="Win")
+            self.new_game()
+        else:
+            enemy_hands = list()
+            for enemy_hand in self.enemy_hands:
+                hand = self.hands[enemy_hand]
+                if len(hand.cards_ids) > 0:
+                    enemy_hands.append(enemy_hand)
+            self.enemy_hands = enemy_hands
+            if (len(self.enemy_hands) == 0):
+                messagebox.showinfo(message="Lose")
+                self.new_game()
+            else:
+                self.enemy_hand = self.enemy_hands[-1]
+                for i, card in enumerate(self.hands[self.enemy_hand].cards):
+                    card.bind('<Button-1>', partial(self.unfold, i))
+                    self.stage = stage
+
+    def pass_turn(self, event=None):
+        """
+        Pass the turn to the counterpart.
+
+        Opponent draws card, removes pairs and passes turn
+        """
         if self.stage == 2:
-            index = np.random.choice(np.arange(len(self.hands[self.player_hand].cards_ids)), 1)[0]
-            card_id = self.hands[self.player_hand].cards_ids[index]
-            self.hands[self.player_hand].cards_ids[index] = None
+            self.draw_list = []
+            for i in range(len(self.enemy_hands) - 1):
+                self.draw_list.append(
+                    [self.enemy_hands[i], self.enemy_hands[i+1]]
+                )
+            self.draw_list.append([self.player_hand, self.enemy_hands[0]])
+            print(self.draw_list)
+            for i, indices in enumerate(self.draw_list):
+                self.master.after(
+                    2000 * i,
+                    self.draw_from_hand,
+                    indices[0],
+                    indices[1]
+                )
+            self.master.after((i + 1) * 2000, self.end_game)
 
-            self.hands[self.enemy_hand].cards_ids.append(card_id)
-            position = self.hands[self.enemy_hand].position(len(self.hands[self.enemy_hand].cards_ids) - 1)
-            for hand in self.hands:
-                hand.show(hand.cards_ids)
-            self.hands[self.enemy_hand].cards[-1].destroy()
-            self.hands[self.enemy_hand].cards[-1] = self.create_button(card_id, position)
-            self.stage = 3
-            self.master.after(500, self.pass_turn)
-        elif self.stage == 3:
-            index = self.hands[self.player_hand].cards_ids.index(None)
-            self.hands[self.player_hand].cards_ids.pop(index)
-            self.remove_pairs(self.hands[self.enemy_hand])
-            np.random.shuffle(self.hands[self.enemy_hand].cards_ids)
-            for hand in self.hands:
-                hand.show(hand.cards_ids)
-            for i, card in enumerate(self.hands[self.enemy_hand].cards):
-                card.bind('<Button-1>', partial(self.unfold, i))
-            self.stage = 0
-            if (len(self.hands[self.enemy_hand].cards_ids) == 0):
-                messagebox.showinfo(message = "Lose")
-                self.new_game()
-            elif (len(self.hands[self.player_hand].cards_ids) == 0):
-                messagebox.showinfo(message = "Win")
-                self.new_game()
-            print(len(self.hands[self.enemy_hand].cards_ids), len(self.hands[self.player_hand].cards_ids))
+    def draw_from_hand(self, index_draw, index_hold):
+        """
+        Start card draw.
 
+        Args:
+            index_draw: index to hand to draw from
+            index_hold: index to hand to draw to
+        """
+        hand_draw = self.hands[index_draw]
+        hand_hold = self.hands[index_hold]
+        if len(hand_hold.cards_ids) > 0:
+            if self.stage == 2:
+                index = np.random.choice(
+                    np.arange(len(hand_draw.cards_ids)), 1)[0]
+                card_id = hand_draw.cards_ids[index]
+                hand_draw.cards_ids[index] = None
+
+                hand_hold.cards_ids.append(card_id)
+                position = hand_hold.position(len(hand_hold.cards_ids) - 1)
+                hand_hold.show(hand_hold.cards_ids)
+                hand_draw.show(hand_draw.cards_ids)
+                hand_hold.cards[-1].destroy()
+                hand_hold.cards[-1] = self.create_button(
+                    card_id,
+                    position
+                )
+                self.stage = 3
+                self.master.after(
+                    500,
+                    self.draw_from_hand,
+                    index_draw,
+                    index_hold,
+                )
+            elif self.stage == 3:
+                index = hand_draw.cards_ids.index(None)
+                hand_draw.cards_ids.pop(index)
+                self.remove_pairs(hand_hold)
+                np.random.shuffle(hand_hold.cards_ids)
+                hand_hold.show(hand_hold.cards_ids)
+                hand_draw.show(hand_draw.cards_ids)
+                self.stage = 2
 
     def new_game(self):
-        self.stage = 0
+        """Reset game parameters and draw hand`s cards."""
+        self.enemy_hands = list(np.delete(np.arange(len(self.hands)), 2))
+        self.enemy_hand = self.enemy_hands[-1]
         self.shuffle()
-        self.deck_cards_ids = np.delete(self.deck_cards_ids, 37) #queen of clubs
-        self.draw(self.hands[self.enemy_hand], 25, drop = True)
-        self.remove_pairs(self.hands[self.enemy_hand])
-        self.hands[self.enemy_hand].show(self.hands[self.enemy_hand].cards_ids)
-        self.draw(self.hands[self.player_hand], 26, drop = True)
-        self.remove_pairs(self.hands[self.player_hand])
-        self.hands[self.player_hand].show(self.hands[self.player_hand].cards_ids)
-        for i, card in enumerate(self.hands[self.enemy_hand].cards):
-            card.bind('<Button-1>', partial(self.unfold, i))
+        self.deck_cards_ids = np.delete(self.deck_cards_ids, 37)
+        for hand, amount in zip(self.hands, [17, 17, 17]):
+            self.draw(hand, amount, drop=True)
+            self.remove_pairs(hand)
+            hand.show(hand.cards_ids)
+
+        self.end_game(0)
+
 
 if __name__ == '__main__':
 
     root = tk.Tk()
-    root.geometry('870x670')
+    root.geometry('870x900')
 
-    H1 = Queen_Hand(root, row = 0, show_cards = False, allow_movement = False)
-    H1.show([])
+    H1 = Queen_Hand(root, row=0, show_cards=False, allow_movement=False)
 
-    H2 = Queen_Hand(root, row = 2, show_cards = True, allow_movement = True)
-    H2.show([])
+    H2 = Queen_Hand(root, row=1, show_cards=False, allow_movement=False)
 
-    Q = Queen(root, row = 1, hands = [H1, H2], player_hand = 1)
+    H3 = Queen_Hand(root, row=3, show_cards=True, allow_movement=True)
+
+    Q = Queen(root, row=2, hands=[H1, H2, H3], player_hand=2)
     root.mainloop()

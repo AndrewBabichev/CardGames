80a6c61bb16a953999c929a1badb450d0fd0aaca
ЕК <evgeniyik9819@gmail.com>
2021-05-23 11:39:56 +0300

    add sever backend

--- mysite/chat/models.py
+++ mysite/chat/models.py
@@ -1,14 +1,23 @@
 from django.db import models
 
 class Room(models.Model):
-    room_id =  models.AutoField(primary_key=True, default=0)
-    room_name = models.CharField(max_length=20)
+    room_name = models.CharField(max_length=50, primary_key=True)
     n_connected = models.IntegerField(default=0)
     n_demanded = models.IntegerField(default=0)
+    n_ready = models.IntegerField(default=0)
+    n_free = models.IntegerField(default=0)
     #time_creation = models.DateTimeField(blank=True)
 
 class User(models.Model):
-    user_id =  models.AutoField(primary_key=True)
-    user_name = models.CharField(max_length=30)
+    class Meta:
+        unique_together = (("user_name", "room_name"),)
+    user_name = models.CharField(max_length=30, primary_key=True)
     #start_time = models.DateTimeField(blank=True)
-    room_id = models.ForeignKey(Room, on_delete=models.CASCADE)
+
+    room_name = models.CharField(max_length=50)
+    user_room_number = models.PositiveSmallIntegerField(default=0)
+    status = models.CharField(max_length=10, default='waiting')
+    num_cards = models.PositiveSmallIntegerField(default=0)
+    score = models.IntegerField(default=0)
+    is_free = models.BooleanField(default=False)
+    #room_id = models.ForeignKey(Room, on_delete=models.CASCADE)
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -13,7 +13,17 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import django
 import dj_database_url
+from django.conf import settings
+
+#import django
+#django.setup()
+
+
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,16 +35,17 @@ DATABASES = { 'default': dj_database_url.config(conn_max_age=500) }
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-dnw$4q0@91p1gny6(v9p2!#)zm7+o(ms#hpji%%r^7!90hdarm'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
+with open(os.path.join(STATIC_ROOT, 'secret_key.txt')) as f:
+    SECRET_KEY = f.read().strip()
 # Application definition
-
+#django.setup()
 INSTALLED_APPS = [
     'channels',
     'chat',
@@ -49,6 +60,8 @@ INSTALLED_APPS = [
 
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'chat.middleware.startup.StartupMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -84,7 +97,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [('127.0.0.1', 6379)],
+            "hosts": [os.environ.get("REDIS_URL")],
         },
     },
 }
@@ -142,4 +155,3 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-

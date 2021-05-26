68b6d970640ced5413b6873adc67244223bacb88
EKolmagorov <evgeniyik9819@gmail.com>
2021-05-26 10:06:36 +0300

    added all needed

--- mysite/chat/routing.py
+++
@@ -1,9 +0,0 @@
-# chat/routing.py
-from django.urls import re_path
-
-from . import consumers
-
-websocket_urlpatterns = [
-    re_path(r'game', consumers.GameConsumer.as_asgi()),
-    re_path(r'chat', consumers.ChatConsumer.as_asgi())
-]

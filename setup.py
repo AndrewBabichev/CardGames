2b9ee629849ef667364601050b8d7dc6b96e6825
AndrewBabichev <79378003+AndrewBabichev@users.noreply.github.com>
2021-05-28 19:03:58 +0300

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,38 +1,40 @@
+"""Setup script."""
 
 import glob
 import os
 
+from setuptools import setup
 
-import setuptools
-from setuptools import setup, find_packages
+
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 
-
-def get_package():
-    return glob.glob("src/resources/**/*", recursive=True)
-
-
 def del_prefix(path):
+    """Delete prefix path."""
     return os.path.relpath(path, "src")
 
 
-packages = list(map(del_prefix, glob.glob("CardGames/resources/**/*", recursive=True)))
-locales =  list(map(del_prefix, glob.glob("CardGames/localization/**/**/*", recursive=True)))
-
+packages = list(map(
+    del_prefix,
+    glob.glob("CardGames/resources/**/*", recursive=True))
+)
+locales = list(map(
+    del_prefix,
+    glob.glob("CardGames/localization/**/**/*", recursive=True))
+)
 
 setup(
-     name='CardGames',
-     version='0.1',
-     author="Eugene Kolmagorov and Andrew Babichev",
-     author_email="appleforopad@inbox.ru",
-     description="Bleckjack, Fool and Queen games",
-     long_description=long_description,
-     long_description_content_type="text/markdown",
-     url="https://github.com/AndrewBabichev/CardGames",
+    name='CardGames',
+    version='0.1',
+    author="Eugene Kolmagorov and Andrew Babichev",
+    author_email="appleforopad@inbox.ru",
+    description="Bleckjack, Fool and Queen games",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/AndrewBabichev/CardGames",
 
-      classifiers=[
+    classifiers=[
         'Development Status :: 4 - Beta',
 
         # Indicate who your project is intended for
@@ -53,16 +55,19 @@ setup(
     packages=['CardGames', 'CardGames.code'],
     install_requires=[
         'playsound>=1.2.2',
-        'numpy>=1.20.1',
+        'numpy>=1.19.5',
         'Pillow>=8.2.0',
         'websocket_client>=1.0.1',
         'websockets==9.0.1',
+        'vext.gi',
+        'vext',
     ],
-     entry_points={
-        "console_scripts":[
+    entry_points={
+        "console_scripts":
+        [
             "cards = CardGames.__main__:main"
         ]
-     },
-     include_package_data=True,
-     package_data = {'':packages + locales}
+    },
+    include_package_data=True,
+    package_data={'': packages + locales}
  )
